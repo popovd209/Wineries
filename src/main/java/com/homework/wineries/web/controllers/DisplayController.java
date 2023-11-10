011d9615cb8b78fbe595f246f402827e808eb967
AndrejDevs <andrejpanev8@gmail.com>
2023-11-10 22:55:42 +0100

    AbsolutePath Faulty push

--- src/main/java/com/homework/wineries/web/controllers/DisplayController.java
+++ src/main/java/com/homework/wineries/web/controllers/DisplayController.java
@@ -3,6 +3,7 @@ package com.homework.wineries.web.controllers;
 import com.homework.wineries.WineriesApplication;
 import com.homework.wineries.web.Pipe;
 import com.homework.wineries.web.filters.ReadLineFilter;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,28 +21,25 @@ import java.util.Scanner;
 public class DisplayController{
 
     @GetMapping
-    public String DisplayPage(Model model) throws FileNotFoundException {
-        List<String> LinesList = new ArrayList<>();
+    public String displayPage(Model model) throws IOException {
+        List<String> linesList = new ArrayList<>();
 
-        //Neccesities//
-        //Scanner scanner = new Scanner(new File("../../resources/wineries_final.csv"));
-        Scanner scanner = new Scanner(new File("C:\\Users\\HP\\Desktop\\FINKI\\III - Semestar 5\\DIANS\\Homework\\Wineries\\src\\main\\resources\\TestingFile.csv"));
-        Pipe<String> pipe = new Pipe<>();
+        // Use ClassPathResource to obtain a File object from a relative path
+        File file = new ClassPathResource("wineries_final.csv").getFile();
 
-        //Filters//
-        ReadLineFilter readLineFilter = new ReadLineFilter();
+        try (Scanner scanner = new Scanner(file)) {
+            Pipe<String> pipe = new Pipe<>();
+            ReadLineFilter readLineFilter = new ReadLineFilter();
+            pipe.addFilter(readLineFilter);
 
-        //Filter injection//
-        pipe.addFilter(readLineFilter);
-
-        while(scanner.hasNextLine()){
-            String line = scanner.nextLine();
-
-            String result = pipe.runFilters(line);
-            LinesList.add(result);
-            model.addAttribute("Display", LinesList);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String result = pipe.runFilters(line);
+                linesList.add(result);
+            }
         }
 
+        model.addAttribute("DisplayLines", linesList);
+
         return "MainDisplay";
-    }
-}
+    }}
