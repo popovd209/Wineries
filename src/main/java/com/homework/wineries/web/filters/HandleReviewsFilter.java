1a57f3a32ff7196ffc72b9cce0cf1bebc1236ea0
AndrejDevs <andrejpanev8@gmail.com>
2023-11-12 00:33:57 +0100

    Fixed missing reviews, some reviews now are mixed up with rating while others are fine, still needs work

--- src/main/java/com/homework/wineries/web/filters/HandleReviewsFilter.java
+++ src/main/java/com/homework/wineries/web/filters/HandleReviewsFilter.java
@@ -1,53 +1,52 @@
 package com.homework.wineries.web.filters;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.homework.wineries.interfaces.IFilter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HandleReviewsFilter implements IFilter<String[]> {
-    public static String fixJson(String jsonString) {
-        return jsonString.replace("'", "\"");
-    }
-    public static List<StringBuilder> parseJson(String jsonString) {
+
+    //TO-DO: Some Ratings are shown mixed up, needs fix
+    public static String parseReviews(String input) {
+        // Use a regex pattern to extract review information
+//        String patternString = "\\{'review_author': '(.*?)', 'rating': '(.*?)', 'text': '(.*?)', 'relative_time_description': '(.*?)'\\}";
+//        String patternString = "\\{'review_author':\\s*'([^']+)',\\s*'rating':\\s*'([^']+)',\\s*'text':\\s*'([^']+)'\\}";
+        String patternString = "\\{'review_author': '(.*?)', 'rating': '(.*?)', 'text': '(.*?)'\\}";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(input);
         List<StringBuilder> formattedReviews = new ArrayList<>();
-        try {
-            // Create an ObjectMapper
-            ObjectMapper objectMapper = new ObjectMapper();
 
-            // Read the JSON string into a JsonNode
-            JsonNode jsonNode = objectMapper.readTree(jsonString);
+        while (matcher.find()) {
+            String reviewAuthor = matcher.group(1);
+            String rating = matcher.group(2);
+            String text = matcher.group(3).replace("\"", ""); // Remove escaped quotes
+//            String relativeTimeDescription = matcher.group(4);
 
-            // Now you can work with the parsed JSON
-            for (JsonNode reviewNode : jsonNode) {
-                String reviewAuthor = reviewNode.get("review_author").asText();
-                String rating = reviewNode.get("rating").asText();
-                String text = reviewNode.get("text").asText().replace("\n", "<br/>"); // Replace newline characters with <br/>
-                String relativeTimeDescription = reviewNode.get("relative_time_description").asText();
+            StringBuilder formattedReview = new StringBuilder();
+            // Formatting the extracted information
+            formattedReview.append("Review Author: ").append(reviewAuthor).append("<br/>");
+            formattedReview.append("Rating: ").append(rating).append("<br/>");
+            formattedReview.append("Text: ").append(text).append("<br/>");
+//            formattedReview.append("Relative Time Description: ").append(relativeTimeDescription).append("<br/>");
+            formattedReview.append("------<br/>");
 
-                StringBuilder formattedReview = new StringBuilder();
-                formattedReview.append("Review Author: ").append(reviewAuthor).append("<br/>");
-                formattedReview.append("Rating: ").append(rating).append("<br/>");
-                formattedReview.append("Text: ").append(text).append("<br/>");
-                formattedReview.append("Relative Time Description: ").append(relativeTimeDescription).append("<br/>");
-                formattedReview.append("------<br/>");
-
-                // Add the formatted review to the list
-                formattedReviews.add(formattedReview);
-            }
-        } catch (Exception e) {
-//            e.printStackTrace();
-            // 36 reviews perish here in this black hole due to bad format unable to fix it with code
-            //Must fix this try separation by {} or smth
+            // Add the formatted review to the list
+            formattedReviews.add(formattedReview);
         }
-        return formattedReviews;
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb : formattedReviews) {
+            result.append(sb.toString());
+        }
+        return result.toString();
     }
-    @Override
+        @Override
     public String[] execute(String[] input) {
         if(!input[12].equals("[]"))
-            input[12] = parseJson(fixJson(input[12])).toString().replace("[","").replace("]","");
+            input[12] = parseReviews(input[12]).replace("[","").replace("]","");
         else
             input[12] = "Not found";
         return input;
